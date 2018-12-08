c2ec2d9355f64ecb2bfedba2b61cf3d2977bb54d
Vincent <vincent@yahoo.com>
2018-12-08 13:28:51 +0800

    Add resources of landing, counter and asset to respective endpoints

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from flask import Flask, Blueprint
 from flask_restful import Api
-from flask_moment import Moment
 from config import config
 from resources import *
-
-moment = Moment()
 
 def create_app(config_name):
   app = Flask(__name__)
@@ -13,18 +10,20 @@ def create_app(config_name):
   app.config.from_object(config_target)
   config_target.init_app(app)
 
-  moment.init_app(app)
-
   api_bp = Blueprint('api', __name__, url_prefix='/api')
   api = Api(api_bp)
 
-  api.add_resource(HomeResource, '/')
+  api.add_resource(LandingResource, '/')
+  api.add_resource(HomeResource, '/home')
+  api.add_resource(CounterResource, '/counter')
+  api.add_resource(AssetResource, '/asset')
 
   app.register_blueprint(api_bp)
+
   return app
 
 config_name = 'testing'
 app = create_app(config_name)
 
 if __name__ == '__main__':
-  app.run(ebug=True, host='0.0.0.0')
+  app.run(debug=True, host='0.0.0.0')
